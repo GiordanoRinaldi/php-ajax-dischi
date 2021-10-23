21819542fa271366848ab8a01ab0370f3ec5a441
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-10-23 15:04:38 +0200

    aggiunto filtraggio

--- api/index.php
+++ api/index.php
@@ -3,4 +3,16 @@ require __DIR__ . "/../database.php";
 
 header("Content-Type: application/json");
 
-echo json_encode($database);
+if(!empty($_GET["genre"]) ) {
+    $genre = $_GET["genre"];
+    $dischi_filtrati = [];
+    foreach($database as $dischi) {
+        if($genre == strtolower($dischi["genre"])){
+            $dischi_filtrati[] = $dischi;
+        }
+    }
+    echo json_encode($dischi_filtrati);
+}else{
+    echo json_encode($database);
+}
+
--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
 const app = new Vue({
     el: "#root",
     data: {
-        dischi: null
-
+        dischi: null,
+        generi: [],
+        genereselezionato: "",
+    },
+    methods: {
+        filtraCanzoni(){
+            axios.get("http://localhost/php-ajax-dischi/api/index.php", {
+                params: {
+                    "genre" : this.genereselezionato.toLowerCase()
+                }
+            })
+        .then( (response) => {
+            this.dischi = response.data;
+            
+        })
+        }
     },
     created(){
         axios.get("http://localhost/php-ajax-dischi/api/index.php")
         .then( (response) => {
             this.dischi = response.data;
+
+            this.dischi.forEach(elm => {
+                if (!this.generi.includes(elm.genre)){
+                    this.generi.push(elm.genre);
+                }
+            });
         })
     }
 })
